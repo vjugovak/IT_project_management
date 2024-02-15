945202564138f372e00650c2478b3b66ed24709c
caapel <caapel@mail.ru>
2024-02-15 12:58:56 +0300

    Create Task Combine

--- Program.cs
+++ Program.cs
@@ -1,4 +1,30 @@
 ﻿namespace IT_project_management
 {
+    class Program
+    {
+        public static void Main()
+        {
+            var ints = new[] { 1, 2 };
+            var strings = new[] { "A", "B" };
 
+            Print(Combine(ints, ints));
+            Print(Combine(ints, ints, ints));
+            Print(Combine(ints));
+            Print(Combine());
+            Print(Combine(strings, strings));
+            Print(Combine(ints, strings));
+        }
+
+        static void Print(Array array)
+        {
+            if (array == null)
+            {
+                Console.WriteLine("null");
+                return;
+            }
+            for (int i = 0; i < array.Length; i++)
+                Console.Write("{0} ", array.GetValue(i));
+            Console.WriteLine();
+        }
+    }
 }
