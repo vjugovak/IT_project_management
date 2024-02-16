5c3d2f558cc253e9147cd7311db9501e616011d6
caapel <caapel@mail.ru>
2024-02-16 12:01:49 +0300

    Create Task PocketGoogle

---
+++ Indexer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PocketGoogle;
+
+public class Indexer : IIndexer
+{
+	public void Add(int id, string documentText)
+	{
+		throw new NotImplementedException();
+	}
+
+	public List<int> GetIds(string word)
+	{
+		throw new NotImplementedException();
+	}
+
+	public List<int> GetPositions(int id, string word)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void Remove(int id)
+	{
+		throw new NotImplementedException();
+	}
+}
